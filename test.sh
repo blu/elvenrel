d754662a759ca803b1c7799c80535bfc7edd71ce
Martin Krastev <blu.dark@gmail.com>
2021-08-22 23:04:41 +0300

    update test script to include heap VMA disposal

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@
 make all
 
 # Load two RELs with cross-relocations; nuke all VMAs originating from common libraries before passing control to _start
-./elvenrel test_cross1.o test_cross2.o --filter /lib/aarch64-linux-gnu --filter /usr/lib/aarch64-linux-gnu
+./elvenrel test_cross1.o test_cross2.o --filter /lib/aarch64-linux-gnu --filter /usr/lib/aarch64-linux-gnu --filter [heap]
