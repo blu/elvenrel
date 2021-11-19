2a45c5b175e66e3676efcb9e7918b7b849bee076
Martin Krastev <blu.dark@gmail.com>
2021-11-19 22:20:40 +0200

    * add non-aligned memset to tests * add test for memset

--- test_common/memset32.s
+++ test_common/memset32.s
@@ -1,9 +1,10 @@
 	.arch armv8-a
 
 	.global memset32
+	.global memset
 	.text
 
-// memset a buffer of preferred 32B alignment to a given value
+// memset a buffer of 32B alignment to a given value
 // x0: buffer
 // x1: buffer + length
 // v0: byte value splatted to ASIMD width
@@ -27,4 +28,33 @@ memset32:
 	str	b0, [x0], 1
 	b	.Lclear_fb_tail_1
 .Lfb_done:
+	ret
+
+// memset a buffer to a given value
+// x0: buffer
+// x1: length
+// v0: byte value splatted to ASIMD width
+// clobbers: x2, x3
+	.align 4
+memset:
+	ands	x3, x1, -32
+	and	x2, x1, -16
+	add	x3, x3, x0
+	add	x2, x2, x0
+	add	x1, x1, x0
+	beq	.LLclear_fb_tail_0
+.LLclear_fb:
+	stp	q0, q0, [x0], 32
+	cmp	x0, x3
+	bne	.LLclear_fb
+.LLclear_fb_tail_0:
+	cmp	x0, x2
+	beq	.LLclear_fb_tail_1
+	str	q0, [x0], 16
+.LLclear_fb_tail_1:
+	cmp	x0, x1
+	beq	.LLfb_done
+	str	b0, [x0], 1
+	b	.LLclear_fb_tail_1
+.LLfb_done:
 	ret
---
+++ test_linux/test_memset.s
@@ -0,0 +1,47 @@
+	.global _start
+
+	.equ SYS_write, 64
+	.equ SYS_exit, 93
+	.equ STDOUT_FILENO, 1
+
+	.equ COLUMNS, 64
+	.equ LINES, 48
+
+// load 'far' address as a +/-4GB offset from PC
+.macro adrf Xn, addr:req
+	adrp	\Xn, \addr
+	add	\Xn, \Xn, :lo12:\addr
+.endm
+
+_start:
+	adrf	x4, fb
+	add	x5, x4, (COLUMNS + 1) * LINES
+	mov	x6, 1
+once:
+	movi	v0.16b, '.'
+	mov	x0, x4
+	mov	x1, x6
+	bl	memset
+
+	add	x6, x6, 1
+	add	x4, x4, COLUMNS + 1
+	cmp	x4, x5
+	bne	once
+
+	mov	x8, SYS_write
+	mov	x2, fb_len
+	adr	x1, fb
+	mov	x0, STDOUT_FILENO
+	svc	0
+
+	mov	x8, SYS_exit
+	mov	x0, xzr
+	svc	0
+
+	.data
+fb:
+	.rept LINES
+	.fill COLUMNS, 1, '='
+	.byte '\n'
+	.endr
+fb_len = . - fb
