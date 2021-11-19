e2ca9c1971020697f8dafb83e5a534387d99720f
Martin Krastev <blu.dark@gmail.com>
2021-11-20 01:19:34 +0200

    refactor memset test start-address computation

--- test_macos/test_memset.s
+++ test_macos/test_memset.s
@@ -12,13 +12,16 @@ _start:
 	add	x4, x4, :lo12:fb
 	add	x5, x4, (COLUMNS + 1) * LINES
 	mov	x6, 1
+	mov	x7, 1
 loop:
 	movi	v0.16b, '.'
 	mov	x0, x4
 	mov	x1, x6
 	bl	memset
 
-	tst	x6, COLUMNS / (COLUMNS - LINES) - 1
+	cmp	x7, LINES / (COLUMNS - LINES)
+	csel	x7, xzr, x7, EQ
+	add	x7, x7, 1
 	add	x6, x6, 1
 	cinc	x4, x4, EQ
 	add	x4, x4, COLUMNS + 1
